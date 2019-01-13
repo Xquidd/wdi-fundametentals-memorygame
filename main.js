fadd72ebd6f09f2a7943976c0bcc5f2e8939da0d
Xquidd <46495595+Xquidd@users.noreply.github.com>
2019-01-13 14:08:47 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User Flipped" + cardOne);
-console.log("User Flipped"+ cardThree);
+console.log("User Flipped" + cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay[1]){
+	alert("You Found A Match");
+} else{
+	alert("Sorry, Try Again");
+}
