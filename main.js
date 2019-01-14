4201fcdea764af93f88d19eab95c1aa37ba29584
Xquidd <46495595+Xquidd@users.noreply.github.com>
2019-01-13 19:05:15 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User Flipped" + cardOne);
-console.log("User Flipped" + cardTwo);
+var checkForMatch = function () {
 if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay[1]){
 	alert("You Found A Match");
 } else{
 	alert("Sorry, Try Again");
 }
+}
+var flipCard = function (cardID) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cardID);
+}
+flipCard(0);
+flipCard(2);
