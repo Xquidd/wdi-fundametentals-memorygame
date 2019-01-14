b8faf4b766c29de4ed04c546db4e3f19ac354494
Xquidd <46495595+Xquidd@users.noreply.github.com>
2019-01-14 17:45:11 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+var cardsInPlay = []
 var checkForMatch = function () {
 if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay[1]){
 	alert("You Found A Match");
@@ -9,8 +31,10 @@ if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay[1]){
 }
 var flipCard = function (cardID) {
 	checkForMatch();
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cardID);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 }
 flipCard(0);
 flipCard(2);
